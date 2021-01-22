5ee87d555f60bf3fc9841b79b1928684976fc2ae
EliottZemour <eliott.zemour@epfl.ch>
2021-01-22 15:10:00 +0100

    Revert "deleting useless files"
    This reverts commit 6cf877f42391b570952333a4d40a61dcf69721f5.

---
+++ src/exc/ConvergenceException.cpp
@@ -0,0 +1,13 @@
+/*
+* Class for Exceptions when problems of convergence
+        *
+        * 	   Author: Cyril Vallez <cyril.vallez@epfl.ch>
+*/
+
+#include "ConvergenceException.hpp"
+#include <string>
+
+ConvergenceException::ConvergenceException (std::string problems_)
+: Exception("Convergence issue", problems_) {}
+
+
