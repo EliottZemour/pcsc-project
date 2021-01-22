00daa2a7037c16ae5f83557a2bb846bb6f4906e8
EliottZemour <eliott.zemour@epfl.ch>
2021-01-22 09:44:01 +0100

    double check test bisection

---
+++ src/Test_Bisection_Solver.cpp
@@ -0,0 +1,39 @@
+/*
+ *
+ * Test of the Bisection Solver
+ * Works with Google Test framework
+ *
+ */
+
+#include "Bisection_Solver.hpp"
+#include <iostream>
+#include <cmath>
+
+
+
+double function_xcube (double x)
+{
+    return x*x*x - 2.;
+}
+
+double function_sinus (double x) {
+    return sin(x) - 1 / 2.;
+}
+
+int main(int argc, char *argv[])
+{
+
+    std::cout.setf(std::ios::scientific);
+    std::cout.setf(std::ios::showpos);
+    std::cout.precision(15);
+
+    double (*func)(double x) = &function_xcube;
+    double res = Solve_Bisection(func, -1., -2.);
+
+    std::cout << "The numerical result is " << res << std::endl;
+    std::cout << "The exact result is " << pow(2., 1./3.) << std::endl;
+    std::cout << "Error = " << fabs(res - pow(2., 1./3.)) << std::endl;
+    return 0;
+
+}
+
